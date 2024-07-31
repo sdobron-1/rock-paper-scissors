ff91c0df88a0bdf99230efbca684e75f94b9703c
Sdebo1 <Steve.Dobron@hotmail.com>
2024-07-31 19:10:19 -0400

    Update GUI changes

--- java.js
+++ java.js
@@ -1,55 +1,71 @@
-let humanScore = 0;
-let computerScore = 0;
-let i = 0;
-playGame();
-function playGame () {
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice === computerChoice) {
-            console.log(`Your ${humanChoice} ties the computer's ${computerChoice}. This is a draw.`);
-        } else if (humanChoice === "rock" && computerChoice === "scissors") {
-            humanScore++;
-            console.log(`Your ${humanChoice} beats the computer's ${computerChoice}. You win!`);
-        } else if (humanChoice === "paper" && computerChoice === "rock") {
-            humanScore++;
-            console.log(`Your ${humanChoice} beats the computer's ${computerChoice}. You win!`);
-        } else if (humanChoice === "scissors" && computerChoice === "paper") {
-            humanScore++;
-            console.log(`Your ${humanChoice} beats the computer's ${computerChoice}. You win!`);
-        } else {
-            computerScore++;
-            console.log(`The computer's ${computerChoice} beats your ${humanChoice}. You lose.`);
+let humanChoice = "";
+let computerChoice = "";
+
+const btn1 = document.getElementById("rock");
+btn1.addEventListener("click", getHumanChoice);
+
+const btn2 = document.getElementById("paper");
+btn2.addEventListener("click", getHumanChoice);
+
+const btn3 = document.getElementById("scissors");
+btn3.addEventListener("click", getHumanChoice);
+
+const scoreBoard = document.querySelector("div");
+
+function getHumanChoice() {
+    if (humanScore != 5 && computerScore !=5) {
+        try {humanChoice =this.value;
+            return;
+        } finally {
+            getComputerChoice();
+            playRound(humanChoice,computerChoice);
         }
-    }
-    if (i < 5) {
-        const computerChoice = getComputerChoice();
-        const humanChoice = getHumanChoice();
-        playRound(humanChoice,computerChoice);
-        i++;
-        playGame();
-    } else
-    console.log(`The final score is Human: ${humanScore}, Computer: ${computerScore}`);    
+   }
+}
+
+function scoreBoard(humanScore,computerScore) {
+
 }
 
 function getComputerChoice() {
     number = Math.random();
-    if (number <0.33) {
-        return "rock";
-    } else if (number >0.33 && number <0.66) {
-        return "paper"; 
-    }else 
-        return "scissors";
+    if (number <=0.45) {
+        computerChoice = "rock";
+        return;
+    } else if (number >0.45 && number <0.75) {
+        computerChoice = "paper";
+        return; 
+    } else 
+        computerChoice = "scissors";
+        return;
 }
 
-function getHumanChoice() {
-    choice = prompt("Choose rock, paper or scissors.");
-    let choiceArray = new Array();
-        choiceArray[0] = "rock";
-        choiceArray[1] = "paper";
-        choiceArray[2] = "scissors";
-    if (choiceArray.includes(choice.toLowerCase())) {
-        return choice.toLowerCase();
-    } else
-        alert("Please enter only rock, paper or scissors.");
-        getHumanChoice();
-        return choice.toLowerCase();
+    function playRound(humanChoice, computerChoice) {
+        let humanScore = 0;
+        let computerScore = 0;
+        if (humanChoice === computerChoice) {
+            scoreBoard.appendChild(document.createTextNode("\n" + `Your ${humanChoice} ties the computer's ${computerChoice}. This is a draw.`));
+        } else if (humanChoice === "rock" && computerChoice === "scissors") {
+            humanScore++;
+            scoreBoard.appendChild(document.createTextNode("\n" + `Your ${humanChoice} beats the computer's ${computerChoice}. You win!`));
+        } else if (humanChoice === "paper" && computerChoice === "rock") {
+            humanScore++;
+            scoreBoard.appendChild(document.createTextNode("\n" + `Your ${humanChoice} beats the computer's ${computerChoice}. You win!`));
+        } else if (humanChoice === "scissors" && computerChoice === "paper") {
+            humanScore++;
+            scoreBoard.appendChild(document.createTextNode("\n" + `Your ${humanChoice} beats the computer's ${computerChoice}. You win!`));
+        } else {
+            computerScore++;
+            scoreBoard.appendChild(document.createTextNode("\n" + `The computer's ${computerChoice} beats your ${humanChoice}. You lose.`));
+            }
     }
+
+//} else if (humanScore === 5) {
+//    scoreBoard.appendChild(document.createTextNode("\n" + `Your score is ${humanScore} and the computer's score is ${computerScore}. You win!`));
+//    humanScore = 0;
+//    computerScore = 0;
+//} else if (computerScore === 5) {
+//    scoreBoard.appendChild(document.createTextNode("\n" + `The computer's score is ${computerScore} and your score is ${humanScore}. You lose.`));
+//    humanScore = 0;
+//    computerScore = 0;
+//}
